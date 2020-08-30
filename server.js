6461d8f56d48d608326b5ca15beee9741371a305
mwitumi21@gmail.com <git>
2020-08-30 20:47:50 +0800

    Updated User Interface

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ io.on('connection', socket => {
 
         socket.on('disconnect', () => {
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        
         })
     })
 })
